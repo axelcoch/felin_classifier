058999688d2cd6f9b782f1ccb4631264dfbe77b4
axelcoch <axel.cochet@edu.esiee.fr>
2024-03-17 20:21:12 +0100

    data reorder

---
+++ data.py
@@ -0,0 +1,11 @@
+PATH = 'images/'
+
+dict_felin = {
+'Neofelis Nebulosa' : ['https://www.monde-animal.fr/fiches-animaux/neofelis-nebulosa/', PATH + 'neofelis-nebulosa.jpg'],
+'Acinonyx Jubatus': ['https://www.monde-animal.fr/fiches-animaux/acinonyx-jubatus/', PATH + 'guepar.jpg'],
+'Panthera Leo': ['https://www.monde-animal.fr/fiches-animaux/panthera-leo/', PATH + 'lion.jpg'],
+'Panthera Tigris': ['https://www.monde-animal.fr/fiches-animaux/panthera-tigris/', PATH + 'tigre.jpg'],
+'Puma Concolor': ['https://www.monde-animal.fr/fiches-animaux/puma-concolor/', PATH + 'puma.jpg'],
+'Panthera Onca': ['https://www.monde-animal.fr/fiches-animaux/panthera-onca/', PATH + 'Jaguar_full.jpg'],
+'Panthera Pardus': ['https://www.monde-animal.fr/fiches-animaux/panthera-pardus/', PATH + 'leopar.jpg']
+}
--- main.py
+++ main.py
@@ -1,18 +1,7 @@
 import streamlit as st
 import numpy as np
 from utils import prediction_felin
-
-PATH = 'images/'
-
-dict_felin = {
-'Neofelis Nebulosa' : ['https://www.monde-animal.fr/fiches-animaux/neofelis-nebulosa/', PATH + 'neofelis-nebulosa.jpg'],
-'Acinonyx Jubatus': ['https://www.monde-animal.fr/fiches-animaux/acinonyx-jubatus/', PATH + 'guepar.jpg'],
-'Panthera Leo': ['https://www.monde-animal.fr/fiches-animaux/panthera-leo/', PATH + 'lion.jpg'],
-'Panthera Tigris': ['https://www.monde-animal.fr/fiches-animaux/panthera-tigris/', PATH + 'tigre.jpg'],
-'Puma Concolor': ['https://www.monde-animal.fr/fiches-animaux/puma-concolor/', PATH + 'puma.jpg'],
-'Panthera Onca': ['https://www.monde-animal.fr/fiches-animaux/panthera-onca/', PATH + 'Jaguar_full.jpg'],
-'Panthera Pardus': ['https://www.monde-animal.fr/fiches-animaux/panthera-pardus/', PATH + 'leopar.jpg']
-}
+from data import dict_felin
 
 def main():
 
